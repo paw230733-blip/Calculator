e70e47369959f538af1532eb7c15e03d7ebc7669
pawan Rajput <paw230733@gmail.com>
2025-10-03 09:52:33 -0700

    This is a interface

---
+++ Calculator/calValue.java
@@ -0,0 +1,19 @@
+package Calculator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class calValue {
+    private double a;
+    private double b;
+    private double c;
+    private double d;
+    private double e;
+    private double f;
+
+}
